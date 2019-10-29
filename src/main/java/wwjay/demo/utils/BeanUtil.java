f61667edd05702708a62065e2e02ec3073a5d2df
wwj <wwjay0918@gmail.com>
2019-10-29 15:37:12 +0800

    修改BeanUtil

--- src/main/java/wwjay/demo/utils/BeanUtil.java
+++ src/main/java/wwjay/demo/utils/BeanUtil.java
@@ -5,8 +5,6 @@ import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 
 import java.beans.FeatureDescriptor;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Stream;
 
 /**
@@ -18,31 +16,6 @@ import java.util.stream.Stream;
 public class BeanUtil {
 
     private BeanUtil() {
-    }
-
-    /**
-     * 将source的List转换为List<target>
-     *
-     * @param source 源对象
-     * @param target 转换的泛型
-     * @param <B>    泛型
-     * @return List<target>
-     */
-    public static <B> List<B> copyListBean(List<?> source, Class<B> target) {
-        if (source == null) {
-            return null;
-        }
-        List<B> targetList = new ArrayList<>(source.size());
-        try {
-            for (Object o : source) {
-                B b = target.getDeclaredConstructor().newInstance();
-                BeanUtils.copyProperties(o, b);
-                targetList.add(b);
-            }
-        } catch (ReflectiveOperationException e) {
-            throw new RuntimeException("复制List的Bean属性时出错", e);
-        }
-        return targetList;
     }
 
     /**
