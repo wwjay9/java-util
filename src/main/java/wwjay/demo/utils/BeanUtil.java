62dfd73d20e61c8edbc22e3216f723eff2a8d778
wwj <wwjay0918@gmail.com>
2019-11-04 19:25:52 +0800

    修改BeanUtil

--- src/main/java/wwjay/demo/utils/BeanUtil.java
+++ src/main/java/wwjay/demo/utils/BeanUtil.java
@@ -5,6 +5,11 @@ import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 
 import java.beans.FeatureDescriptor;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -12,10 +17,54 @@ import java.util.stream.Stream;
  *
  * @author wwj
  */
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "WeakerAccess"})
 public class BeanUtil {
 
     private BeanUtil() {
+    }
+
+    /**
+     * 将一个List拷贝为另一个List
+     *
+     * @param source   源List
+     * @param supplier 新类型生成器
+     * @return 新类型List
+     */
+    public static <T> List<T> copyList(List<T> source, Supplier<T> supplier) {
+        if (source == null) {
+            return null;
+        }
+        return source.stream().map(copyFunction(supplier)).collect(Collectors.toList());
+    }
+
+    /**
+     * 将一个对象拷贝为另一个对象
+     *
+     * @param supplier 新类型生成器
+     * @return 新类型List
+     */
+    public static <T, R> Function<T, R> copyFunction(Supplier<R> supplier) {
+        return copyFunction(supplier, (t, r) -> {
+        });
+    }
+
+    /**
+     * 将一个对象拷贝为另一个对象
+     *
+     * @param supplier 新类型生成器
+     * @param addition 拷贝对象后的额外操作
+     * @return 新类型List
+     */
+    public static <T, R> Function<T, R> copyFunction(Supplier<R> supplier, BiConsumer<T, R> addition) {
+        return t -> {
+            if (t == null) {
+                return null;
+            }
+            R r = supplier.get();
+            BeanUtils.copyProperties(t, r);
+            addition.accept(t, r);
+            return r;
+        };
     }
 
     /**
