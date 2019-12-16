2adfd3b69d537719619331dbb419ab0664aadff4
wwj <wwjay0918@gmail.com>
2019-12-16 19:27:52 +0800

    增加时间工具类方法

--- src/main/java/wwjay/demo/utils/TimeUtil.java
+++ src/main/java/wwjay/demo/utils/TimeUtil.java
@@ -1,14 +1,16 @@
 package wwjay.demo.utils;
 
 import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 时间相关工具
@@ -83,5 +85,33 @@ public class TimeUtil {
         }
         long s = duration.toSeconds();
         return String.format("%d:%02d:%02d", s / 3600, (s % 3600) / 60, (s % 60));
+    }
+
+    /**
+     * 列出开始时间到结束时间之前所有的月份
+     *
+     * @param start 开始时间
+     * @param end   结束时间
+     * @return 包含开始时间和结束时间的月份
+     */
+    public static List<YearMonth> between(YearMonth start, YearMonth end) {
+        Assert.isTrue(start.compareTo(end) <= 0, "开始时间必须在结束时间之前");
+        return Stream.iterate(start, date -> date.plusMonths(1))
+                .limit(ChronoUnit.MONTHS.between(start, end) + 1)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * 列出开始时间到结束时间之前所有的日期
+     *
+     * @param start 开始时间
+     * @param end   结束时间
+     * @return 包含开始时间和结束时间的日期
+     */
+    public static List<LocalDate> between(LocalDate start, LocalDate end) {
+        Assert.isTrue(start.compareTo(end) <= 0, "开始时间必须在结束时间之前");
+        return Stream.iterate(start, date -> date.plusDays(1))
+                .limit(ChronoUnit.DAYS.between(start, end) + 1)
+                .collect(Collectors.toList());
     }
 }
