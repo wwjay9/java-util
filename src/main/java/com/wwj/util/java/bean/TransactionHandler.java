a209c9cc16c3b8d2b0363ec9833ea4e37ee02c9a
wwj <wwjay0918@gmail.com>
2022-02-18 13:09:06 +0800

    新增事务方法

---
+++ src/main/java/com/wwj/util/java/bean/TransactionHandler.java
@@ -0,0 +1,32 @@
+package com.tuhu.service.jv.handler;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.function.Supplier;
+
+/**
+ * 事务处理程序，用于在service层调用本地事务方法
+ *
+ * @author wwj
+ */
+@Service
+public class TransactionHandler {
+
+    /**
+     * 在当前事务调用
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public <T> T runInTransaction(Supplier<T> supplier) {
+        return supplier.get();
+    }
+
+    /**
+     * 新建一个事务
+     */
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public <T> T runInNewTransaction(Supplier<T> supplier) {
+        return supplier.get();
+    }
+}
